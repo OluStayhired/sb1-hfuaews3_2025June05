f66fe819673c536d8090d8f9da465ed5b7a3bad2
OluStayhired <olu@prelo.io>
2025-07-30 14:19:11 +0100

    fix ShowContentCalendar Navifagtion issue

--- src/components/ShowCalendarContent.tsx
+++ src/components/ShowCalendarContent.tsx
@@ -1124,8 +1124,8 @@ const getScheduleButtonTooltip = () => {
             <p className="text-gray-400 mb-4 text-sm">Create a campaign to get started</p>
           
           <button
-                  //onClick={handleBackToCalendarList}
-                  onClick={handleShowCampaignList}
+                  onClick={handleBackToCalendarList}
+                  //onClick={handleShowCampaignList}
                   className="inline-flex items-center px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors"
                 >
                   <ArrowLeft className="w-5 h-5 mr-2" />
@@ -1145,8 +1145,8 @@ const getScheduleButtonTooltip = () => {
       {/* Add the X button here */}
             <div className="flex justify-start mb-4 mt-[-16px]">
                 <button
-                    //onClick={handleBackToCalendarList} 
-                    onClick={handleShowCampaignList}
+                    onClick={handleBackToCalendarList} 
+                    //onClick={handleShowCampaignList}
                   // Make sure you have this function or state handler
                     className="p-2 bg-gray-100 hover:bg-gray-200 rounded-full transition-colors"
                     title="Close"
