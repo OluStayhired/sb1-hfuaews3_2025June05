d981bb8c3f8cff0b5fbd792a525477803b02b550
OluStayhired <olu@prelo.io>
2025-07-31 09:22:05 +0100

    Tooltip to Activate Post Ideas in CalendarList

--- src/components/CalendarList.tsx
+++ src/components/CalendarList.tsx
@@ -440,8 +440,11 @@ const handleDeleteCampaign = async (calendarName: string) => {
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-xs text-gray-500">
 
+              <TooltipExtended text="⚡Activate campaign for post ideas" className="z-10000 whitespace-wrap">  
+
                   <div className="flex justify-center items-center h-full">
-                    
+
+                  
                   <label 
                     className="relative inline-flex items-center cursor-pointer"
                     onClick={(e) => e.stopPropagation()}
@@ -467,8 +470,11 @@ const handleDeleteCampaign = async (calendarName: string) => {
                     />
                     <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
                   </label>
+                        
                     
                   </div>
+
+                </TooltipExtended>   
                   
                 </td>
                 <td className="px-6 py-6 whitespace-nowrap text-xs text-gray-500">
