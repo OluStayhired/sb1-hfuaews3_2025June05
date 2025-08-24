000083c7c5eb98fe44a0398ed6902d93312ad16f
OluStayhired <olu@prelo.io>
2025-08-24 12:57:15 +0100

    Final fix

--- src/components/CalendarListSidePanel.tsx
+++ src/components/CalendarListSidePanel.tsx
@@ -334,6 +334,7 @@ export function CalendarListSidePanel({ isOpen, onClose, onBackToList, onSelectC
           onCreateNewCampaign={handleCreateNewCampaignFromModal}
       />
     </div>
+    
     </>
   );
 }
